fc2612c0af7dd7a97fcd9c70f24aa9892cb5ce75
Test <d.ndn.g.zm.s.n.fsmb.t@gmail.com>
2025-01-16 12:47:28 +0700

    added base files

---
+++ CarEngineSimulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEngineSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
