1f677219891110f59986474b8e017c81fec4cf9a
Test <d.ndn.g.zm.s.n.fsmb.t@gmail.com>
2025-01-21 11:49:15 +0700

    create Engine

---
+++ CarEngineSimulator/Engine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEngineSimulator
+{
+    abstract class Engine
+    {
+        protected float _i; //Момент инерции двигателя
+        protected List<int> _m; //Кусочно-линейная зависимость крутящего момен
+        protected List<int> _v; //Скорость вращения коленвала
+        protected float _t; //Температура перегрева
+        protected float _hm; //Коэффициент зависимости скорости нагрева от крутящего момента
+        protected float _hv; //Коэффициент зависимости скорости нагрева от скорости вращения коленвала
+        protected float _c; //Коэффициент зависимости скорости охлаждения
+
+        public abstract float EngineTemp { get; } //Температура двигателя
+        public abstract float MaxTemp { get; } //Температура перегрева
+        public abstract bool EngineStatus { get; } //Работа двигателя
+
+        public abstract void Start(float TAir); //Метод запуска двигателя
+        public abstract void Stop(); //Метод остановки двигателя
+    }
+}
---
+++ CarEngineSimulator/ICE.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace CarEngineSimulator
+{
+    class ICE : Engine
+    {
+        private float _curTemp = 0;
+        private bool _isWorking = false;
+        public override float EngineTemp => _curTemp; //Текущая температура
+
+        public override float MaxTemp => _t; //Температура перегрева
+
+        public override bool EngineStatus => _isWorking; //Состояние работы двигателя
+
+        public ICE(float I, List<int> M, List<int> V, float T, float Hm, float Hv, float C)
+        {
+            _i = I;
+            _m = M;
+            _v = V;
+            _t = T;
+            _hm = Hm;
+            _hv = Hv;   
+            _c = C;
+        }
+        public override void Start(float TAir)
+        {
+            float a, Vh, Vc, M, V;
+
+            for (int i = 0; i < _m.Count; i++)
+            {
+                M = _m[i];
+                V = _v[i];
+
+                while (V < _v[i + 1])
+                {
+                    a = M * _i;
+                    Vh = M * _hm + V * V * _hv;
+                    Vc = _c * (TAir - _curTemp);
+
+                    _curTemp += Vh - Vc;
+                    V += a;
+                    M = (V - _v[i]) * (_m[i + 1] - _m[i]) / (_v[i + 1] - _v[i]) + _m[i];
+
+                    if (!_isWorking) return;
+                }
+            }
+        }
+
+        public override void Stop() => _isWorking = false;
+    }
+}
